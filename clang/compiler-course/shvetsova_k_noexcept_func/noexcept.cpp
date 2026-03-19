8dd9f9430cf812b56f7033b6dfe8f83caa570298
Ksuny <xenchick01042005@gmail.com>
2026-03-19 09:45:04 +0300

    add noexcept, fix tests

--- clang/compiler-course/shvetsova_k_noexcept_func/noexcept.cpp
+++ clang/compiler-course/shvetsova_k_noexcept_func/noexcept.cpp
@@ -1,110 +1,206 @@
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ExprCXX.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/Type.h"
-#include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Lex/Lexer.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/Support/raw_ostream.h"
+
+#include <vector>
 
 namespace {
 
-class ThrowVisitor : public clang::RecursiveASTVisitor<ThrowVisitor> {
+class NoexceptScanner final
+    : public clang::RecursiveASTVisitor<NoexceptScanner> {
 public:
-  bool hasThrow = false;
+  NoexceptScanner(const clang::FunctionDecl *current,
+                  const llvm::DenseMap<const clang::FunctionDecl *, bool> &safe)
+      : m_current(current), m_safe(safe) {}
 
   bool VisitCXXThrowExpr(clang::CXXThrowExpr *) {
-    hasThrow = true;
+    m_noexcept = false;
     return false;
   }
+
+  bool VisitCXXTryStmt(clang::CXXTryStmt *) {
+    m_noexcept = false;
+    return false;
+  }
+
+  bool VisitCXXConstructExpr(clang::CXXConstructExpr *) {
+    m_noexcept = false;
+    return false;
+  }
+
+  bool VisitCallExpr(clang::CallExpr *call) {
+    const auto *callee = call->getDirectCallee();
+    if (!callee) {
+      m_noexcept = false;
+      return false;
+    }
+
+    if (callee == m_current)
+      return true;
+
+    const auto it = m_safe.find(callee);
+    if (it != m_safe.end() && it->second)
+      return true;
+
+    const auto *fpt = callee->getType()->getAs<clang::FunctionProtoType>();
+    if (fpt && fpt->hasNoexceptExceptionSpec() && !fpt->getNoexceptExpr())
+      return true;
+
+    m_noexcept = false;
+    return false;
+  }
+
+  bool isNoexcept() const { return m_noexcept; }
+
+private:
+  const clang::FunctionDecl *m_current;
+  const llvm::DenseMap<const clang::FunctionDecl *, bool> &m_safe;
+  bool m_noexcept = true;
 };
 
 class NoexceptVisitor final
     : public clang::RecursiveASTVisitor<NoexceptVisitor> {
-
 public:
-  explicit NoexceptVisitor(clang::ASTContext *context) : context(context) {}
+  explicit NoexceptVisitor(std::vector<clang::FunctionDecl *> &functions)
+      : m_functions(functions) {}
 
   bool VisitFunctionDecl(clang::FunctionDecl *func) {
-
-    auto &SM = context->getSourceManager();
-
-    if (SM.isInSystemHeader(func->getLocation()))
-      return true;
-
-    if (!func->hasBody())
-      return true;
-
-    if (llvm::isa<clang::CXXConstructorDecl>(func))
-      return true;
-
-    if (llvm::isa<clang::CXXDestructorDecl>(func))
-      return true;
-
-    if (func->isOverloadedOperator())
-      return true;
-
-    if (auto *method = llvm::dyn_cast<clang::CXXMethodDecl>(func)) {
-      if (method->isVirtual())
-        return true;
-    }
-
-    if (func->isTemplated())
-      return true;
-
-    const auto *type = func->getType()->getAs<clang::FunctionProtoType>();
-
-    if (!type)
-      return true;
-
-    if (type->isNothrow())
-      return true;
-
-    ThrowVisitor throwVisitor;
-    throwVisitor.TraverseStmt(func->getBody());
-
-    if (throwVisitor.hasThrow)
-      return true;
-
-    llvm::errs() << "Function can be marked noexcept: "
-                 << func->getQualifiedNameAsString() << " at ";
-
-    func->getLocation().print(llvm::errs(), SM);
-    llvm::errs() << "\n";
-
+    if (func && func->hasBody() && !func->isImplicit())
+      m_functions.push_back(func);
     return true;
   }
 
 private:
-  clang::ASTContext *context;
+  std::vector<clang::FunctionDecl *> &m_functions;
 };
 
 class NoexceptConsumer final : public clang::ASTConsumer {
 public:
-  explicit NoexceptConsumer(clang::ASTContext *context) : visitor(context) {}
+  NoexceptConsumer(clang::ASTContext &context, clang::Rewriter &rewriter)
+      : m_context(context), m_rewriter(rewriter), m_visitor(m_functions) {}
 
   void HandleTranslationUnit(clang::ASTContext &context) override {
-    visitor.TraverseDecl(context.getTranslationUnitDecl());
+    m_functions.clear();
+    m_safe.clear();
+
+    m_visitor.TraverseDecl(context.getTranslationUnitDecl());
+
+    for (auto *func : m_functions) {
+      if (isAlreadyNoexcept(func))
+        m_safe[func] = true;
+    }
+
+    bool changed = true;
+    while (changed) {
+      changed = false;
+      for (auto *func : m_functions) {
+        if (m_safe[func])
+          continue;
+        if (functionDoesNotThrow(func)) {
+          m_safe[func] = true;
+          changed = true;
+        }
+      }
+    }
+
+    for (auto *func : m_functions) {
+      if (!m_safe[func])
+        continue;
+      if (isAlreadyNoexcept(func))
+        continue;
+      addNoexcept(func);
+    }
+
+    emitRewrittenSource();
   }
 
 private:
-  NoexceptVisitor visitor;
+  bool isAlreadyNoexcept(const clang::FunctionDecl *func) const {
+    const auto *fpt = func->getType()->getAs<clang::FunctionProtoType>();
+    if (!fpt)
+      return false;
+
+    if (fpt->getExceptionSpecType() == clang::EST_DynamicNone)
+      return true;
+
+    if (fpt->hasNoexceptExceptionSpec() && !fpt->getNoexceptExpr())
+      return true;
+
+    return false;
+  }
+
+  bool functionDoesNotThrow(const clang::FunctionDecl *func) const {
+    const clang::Stmt *body = func->getBody();
+    if (!body)
+      return false;
+
+    NoexceptScanner scanner(func, m_safe);
+    scanner.TraverseStmt(const_cast<clang::Stmt *>(body));
+    return scanner.isNoexcept();
+  }
+
+  void addNoexcept(const clang::FunctionDecl *func) {
+    const auto *body = clang::dyn_cast<clang::CompoundStmt>(func->getBody());
+    if (!body)
+      return;
+
+    clang::SourceLocation braceLoc = body->getLBracLoc();
+    braceLoc = m_context.getSourceManager().getSpellingLoc(braceLoc);
+
+    if (braceLoc.isInvalid())
+      return;
+
+    if (!m_context.getSourceManager().isWrittenInMainFile(braceLoc))
+      return;
+
+    m_rewriter.InsertTextBefore(braceLoc, " noexcept");
+  }
+
+  void emitRewrittenSource() {
+    clang::SourceManager &sm = m_rewriter.getSourceMgr();
+    const clang::FileID mainFile = sm.getMainFileID();
+
+    if (const auto *rewriteBuf = m_rewriter.getRewriteBufferFor(mainFile)) {
+      rewriteBuf->write(llvm::outs());
+      return;
+    }
+
+    llvm::outs() << sm.getBufferData(mainFile);
+  }
+
+private:
+  clang::ASTContext &m_context;
+  clang::Rewriter &m_rewriter;
+  std::vector<clang::FunctionDecl *> m_functions;
+  llvm::DenseMap<const clang::FunctionDecl *, bool> m_safe;
+  NoexceptVisitor m_visitor;
 };
 
 class NoexceptAction final : public clang::PluginASTAction {
 public:
   std::unique_ptr<clang::ASTConsumer>
-  CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef) override {
-
-    return std::make_unique<NoexceptConsumer>(&CI.getASTContext());
+  CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
+    m_rewriter.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
+    return std::make_unique<NoexceptConsumer>(ci.getASTContext(), m_rewriter);
   }
 
-  bool ParseArgs(const clang::CompilerInstance &CI,
-                 const std::vector<std::string> &args) override {
+  bool ParseArgs(const clang::CompilerInstance &,
+                 const std::vector<std::string> &) override {
     return true;
   }
+
+private:
+  clang::Rewriter m_rewriter;
 };
 
 } // namespace
 
 static clang::FrontendPluginRegistry::Add<NoexceptAction>
-    X("noexcept_plugin", "Find functions that can be marked noexcept");
+    X("noexcept_plugin", "Add noexcept to functions that do not throw");
--- clang/test/compiler-course/shvetsova_k_noexcept/noexcept_test.cpp
+++ clang/test/compiler-course/shvetsova_k_noexcept/noexcept_test.cpp
@@ -1,181 +1,33 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/shvetsova_k_noexcept_func_ClangAST%pluginext -plugin noexcept_plugin -fsyntax-only -fcxx-exceptions %s 2>&1 | FileCheck %s
 
-
-// CHECK: Function can be marked noexcept: simpleSafeFunction
-// CHECK: Function can be marked noexcept: complexSafeFunction
-// CHECK: Function can be marked noexcept: safeFunctionWithLoops
-// CHECK: Function can be marked noexcept: functionWithConditions
-// CHECK: Function can be marked noexcept: functionWithSwitch
-// CHECK: Function can be marked noexcept: functionA
-// CHECK: Function can be marked noexcept: functionB
-// CHECK: Function can be marked noexcept: main
-// CHECK: Function can be marked noexcept: TestClass::staticFunction
-
-int simpleSafeFunction() {
-    return 42;
+int square(int value){
+  return value * value;
 }
 
-int complexSafeFunction() {
-    int a = 10;
-    int b = 20;
-    return a + b;
-} 
-
-int safeFunctionWithLoops() {
-    int sum = 0;
-    for(int i = 0; i < 10; ++i) {
-        sum += i;
-    }
-    return sum;
+bool isEven(int value){
+  return value % 2 == 0;
 }
 
-int functionWithConditions(int x) {
-    if (x > 0) {
-        return x;
-    } else {
-        return -x;
-    }
+int already(int value) noexcept {
+  return value + 1;
 }
 
-int functionWithSwitch(int x) {
-    switch(x) {
-        case 1: return 10;
-        case 2: return 20;
-        default: return 0;
-    }
+void throwing(){
+  throw 1;
 }
 
-namespace Outer {
-    namespace Inner {
-        int namespacedFunction() {
-            return 100;
-        }
-    }
-    
-    int outerFunction() {
-        return Inner::namespacedFunction();
-    }
-}
+// CHECK: {{^int square\(int value\) noexcept\{$}}
+// CHECK-NEXT: {{^ *return value \* value;$}}
+// CHECK-NEXT: {{^}$}}
 
-int functionA() { return 1; }
+// CHECK: {{^bool isEven\(int value\) noexcept\{$}}
+// CHECK-NEXT: {{^ *return value % 2 == 0;$}}
+// CHECK-NEXT: {{^}$}}
 
-int functionB() { return functionA() + 1; }
+// CHECK: {{^int already\(int value\) noexcept \{$}}
+// CHECK-NEXT: {{^ *return value \+ 1;$}}
+// CHECK-NEXT: {{^}$}}
 
-int main() {
-    safeFunctionWithLoops();
-    functionWithConditions(10);
-    return 0;
-}
-
-// CHECK-NOT: Function can be marked noexcept: throwingFunction
-int throwingFunction() {
-    throw 1;
-    return 0;
-}
-
-// CHECK-NOT: Function can be marked noexcept: functionWithNestedThrow
-void nestedThrowHelper() {
-    throw 42;
-}
-
-int functionWithNestedThrow() {
-    nestedThrowHelper();
-    return 0;
-}
-
-// CHECK-NOT: Function can be marked noexcept: alreadyNoexcept
-int alreadyNoexcept() noexcept {
-    return 42;
-}
-
-// CHECK-NOT: Function can be marked noexcept: tryCatchFunction
-int tryCatchFunction() {
-    try {
-        throw 1;
-    } catch (...) {
-        return 0;
-    }
-    return 42;
-}
-
-// CHECK-NOT: Function can be marked noexcept: functionWithRethrow
-void functionWithRethrow() {
-    try {
-        throw 1;
-    } catch (...) {
-        throw;
-    }
-}
-
-namespace Outer {
-    namespace Inner {
-        // CHECK-NOT: Function can be marked noexcept: Outer::Inner::throwingNamespacedFunction
-        int throwingNamespacedFunction() {
-            throw 200;
-            return 0;
-        }
-    }
-}
-
-// CHECK-NOT: Function can be marked noexcept: functionC
-int functionC() { throw 1; return 0; }
-
-
-class TestClass {
-public:
-    // CHECK-NOT: Function can be marked noexcept: TestClass
-    TestClass() {}
-    
-    // CHECK-NOT: Function can be marked noexcept: ~TestClass
-    ~TestClass() {}
-    
-    // CHECK-NOT: Function can be marked noexcept: virtualFunction
-    virtual int virtualFunction() {
-        return 42;
-    }
-    
-    // CHECK-NOT: Function can be marked noexcept: operator+
-    TestClass operator+(const TestClass& other) const {
-        return TestClass();
-    }
-    
-    static int staticFunction() {
-        return 100;
-    }
-};
-
-
-struct Number {
-    int value;
-    
-    // CHECK-NOT: Function can be marked noexcept: operator+
-    Number operator+(const Number& other) const {
-        return Number{value + other.value};
-    }
-    
-    // CHECK-NOT: Function can be marked noexcept: operator-
-    Number operator-(const Number& other) const {
-        return Number{value - other.value};
-    }
-};
-
-
-// CHECK-NOT: Function can be marked noexcept: templateFunction
-template<typename T>
-T templateFunction(T a, T b) {
-    return a + b;
-}
-
-// CHECK-NOT: Function can be marked noexcept: templateWithThrow
-template<typename T>
-T templateWithThrow(T a, T b) {
-    if (a == 0) throw 1;
-    return a + b;
-}
-
-
-// CHECK-NOT: Function can be marked noexcept: declarationOnly
-int declarationOnly();
-
-// CHECK-NOT: Function can be marked noexcept: externFunction
-extern void externFunction();
+// CHECK: {{^void throwing\(\)\{$}}
+// CHECK-NEXT: {{^ *throw 1;$}}
+// CHECK-NEXT: {{^}$}}
